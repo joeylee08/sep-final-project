24d0d7797bfc523449d71adf5f3f5651235ee20b
Joseph <josephylee72@gmail.com>
2023-08-10 01:34:57 -0700

    added event listeners and messages

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 let doggies = document.querySelectorAll(".doggy");
 
 doggies.forEach(item => {
-  item.addEventListener("mouseover", function() {
+  item.addEventListener("mouseover", function(e) {
     item.classList.add("doggyhover");
+    showMessage(e);
   })
-  item.addEventListener("mouseout", function() {
+  item.addEventListener("mouseout", function(e) {
     item.classList.remove("doggyhover");
+    hideMessage();
   })
 })
+
+function showMessage(e) {
+  let id;
+
+  switch(e.target.id) {
+    case "doggy1":
+      id = "msg1";
+      break;
+    case "doggy2":
+      id = "msg2";
+      break;
+    case "doggy3":
+      id = "msg3";
+      break;
+    case "doggy4":
+      id = "msg4";
+      break;
+  }
+
+  document.querySelector(`#${id}`).classList.add("unhide");
+}
+
+function hideMessage(e) {
+  document.querySelectorAll(".message").forEach(item => {
+    item.classList.remove("unhide")
+  })
+}
